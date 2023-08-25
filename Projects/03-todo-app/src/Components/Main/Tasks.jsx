ffa03a1445bcb5e0f0598fa85f6fa35d3dbb4df6
Yordy <yoalmonte.dev@outlook.com>
2023-08-25 04:51:39 -0400

    Little style fixed

--- Projects/03-todo-app/src/Components/Main/Tasks.jsx
+++ Projects/03-todo-app/src/Components/Main/Tasks.jsx
@@ -2,7 +2,7 @@ export function Task(props) {
   return (
     <li className="
       flex flex-row px-3 py-2 justify-around items-center max-w-[90%] h-auto
-      bg-gray-300 rounded-xl text-center gap-2 tracking-tight
+      bg-gray-300 rounded-md text-center gap-2 tracking-tight
       leading-5 border-2 border-sky-500 text-black
     ">
       <span>x</span>
