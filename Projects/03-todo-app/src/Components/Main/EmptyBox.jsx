7b518d7a82582db7b8cdb3babd7790c6cd08d462
Yordy Almonte <yoalmonte.dev@outlook.com>
2024-01-14 18:53:10 -0400

    Hotfix!

--- Projects/03-todo-app/src/Components/Main/EmptyBox.jsx
+++ Projects/03-todo-app/src/Components/Main/EmptyBox.jsx
@@ -1,5 +1,5 @@
 export function EmptyBox() {
   return (
-    <p>Add a new task!</p>
+    <p className="flex justify-center px-3 py-2 text-lg">Add a new task!</p>
   )
 }
--- Projects/03-todo-app/src/Components/Main/NoResults.jsx
+++ Projects/03-todo-app/src/Components/Main/NoResults.jsx
@@ -1,5 +1,5 @@
 export function NoResults() {
   return (
-    <p>404</p>
+    <p className="flex justify-center px-3 py-2 text-lg">404</p>
   )
 }
